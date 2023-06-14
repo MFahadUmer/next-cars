80e03ee4775140e454312872632f7a1391def08d
Muhammad Fahad Umer <eng.fahadumer@gmail.com>
2023-06-14 20:42:18 +0500

    Adds Hero Section

---
+++ components/CustomButton.js
@@ -0,0 +1,17 @@
+'use client';
+import React from 'react';
+
+const CustomButton = ({ title, containerStyles, handleClick }) => {
+  return (
+    <button
+      disabled={false}
+      type={'button'}
+      className={`custom-btn ${containerStyles}`}
+      onClick={handleClick}
+    >
+      <span className={`flex-1`}>{title}</span>
+    </button>
+  );
+};
+
+export default CustomButton;
